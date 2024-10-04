0048fe864aca9a873dd75caccda6b072e75c7393
Nathan Regaudie <nathan.regaudie9@etu.univ-lorraine.fr>
2024-10-04 20:19:38 +0200

    Création du modèle User pour la base de données

---
+++ src/database/models/user.js
@@ -0,0 +1,38 @@
+const mangoose = require('mongoose');
+
+const userSchema = new mangoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    pseudo: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    personalCategories: {
+        type: [String],
+        default: [],
+    },
+    agendas: {
+        type: [String],
+        default: [],
+    }
+});
+
+const User = mangoose.model('User', userSchema);
+
+module.exports = User;
