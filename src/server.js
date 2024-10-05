a9495f7d8821090ef3f55526ab167995b1d42503
Nathan Regaudie <nathan.regaudie9@etu.univ-lorraine.fr>
2024-10-05 12:30:18 +0200

    Sauvegarde de la session d'un utilisateur connecté

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const flash = require('connect-flash');
+const MangoStore = require('connect-mongo');
 const path = require('path');
 
 const routes = require('./routes');
@@ -26,26 +26,41 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Configuration des sessions
+const store  = MangoStore.create({
+  mongoUrl: 'mongodb://localhost:27017/db_ulagenda',
+  ttl: 2 * 60 * 60, // Durée de validité de la session: 2 heures
+  collectionName: 'sessions',
+  autoRemove: 'interval',
+  autoRemoveInterval: 10,
+});
+
 app.use(session({
   secret: '8b6d27cea47cc8732f8e32b912c4e149a38f46ac68d95ac0886d84e9ae39400ec8c4745731c0e5fa100cd78be62050ae3ec258dc851a0e95d96b3a70e36c9c1c',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // false car on n'est pas en HTTPS
+  resave: true,
+  rolling: true,
+  saveUninitialized: false,
+  store,
+  cookie: {
+    maxAge: 2 * 60 * 60 * 1000, // Durée de validité du cookie: 2 heures
+    httpOnly: true,
+    secure: false // false en HTTP, true en HTTPS
+  },
 }));
 
-
-// Configuration des messages flash
-app
-  .use(flash())
-  .use((req, res, next) => {
-    res.locals.messages = req.flash();  
-    next();
+// Configuration des variables res.locals
+app.use((req, res, next) => {
+  res.locals.messagesFlash = [];
+  if (req.session.messagesFlash) {
+    res.locals.messagesFlash = req.session.messagesFlash;
+    req.session.messagesFlash = []; // Réinitialisation des messages flash
+  }
+  res.locals.user = req.session.user || null; 
+  next();
 });
+
 
 // Route de base
-app.get('/', (req, res) => {
-  res.send('Welcome to ULAgenda!');
-});
+app.get('/', (req, res) => res.render('index'));
 
 // Routes pour afficher le formulaire d'inscription
 app
