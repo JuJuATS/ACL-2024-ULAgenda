ffe262b530b2cc4421cc3becaa3a6293f1423971
tompotio <tomclem.ait@gmail.com>
2024-10-12 16:51:03 +0200

    correction

--- src/routes/agendas/agendas.js
+++ src/routes/agendas/agendas.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('agendas'));
+router.get('/', (req, res) => {
+    res.render('agendas');
+});
 
 router.post('/', async (req, res) => {
     try {
