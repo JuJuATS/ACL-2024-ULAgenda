ba20dae4cdf61a2cc4b5e6760c01329cae08790f
Nathan Regaudie <nathan.regaudie9@etu.univ-lorraine.fr>
2024-10-18 20:45:12 +0200

    correction isAuthentified

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 // MiddleWare qui permet de vérifier qu'un utilisateur est connecté.
 const isAuthentified = (req, res, next) => {
     if (!req.session.isLoggedIn) {
-        return res.redirect("signin");
+        return res.redirect("/signin");
     }
     next();
 }
