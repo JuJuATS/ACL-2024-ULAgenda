27f34b736e7e48191e36a7b104766ad136fa27b9
Nathan Regaudie <nathan.regaudie9@etu.univ-lorraine.fr>
2024-10-22 17:50:21 +0200

    correction chargement environnement

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@
  * ==================================================
  */
 
-require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-});
+require('dotenv').config();
 
 
 
