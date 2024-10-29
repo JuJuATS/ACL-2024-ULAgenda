e56aa793777a31f2d702bf926cc2f91a74440ea0
Neil <82025251+Vopexx@users.noreply.github.com>
2024-10-29 22:39:10 +0100

    Ajout des récurrences pour les rdv

--- src/database/models/rdv.js
+++ src/database/models/rdv.js
@@ -5,9 +5,9 @@ const rdvSchema = new mongoose.Schema({
     dateDebut: { type: Date, required: true },
     dateFin: { type: Date, required: true },
     agendaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agenda', required: true },
-    recurrences: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recurrence' }],
+    recurrences: { type: mongoose.Schema.Types.ObjectId, ref: 'Recurrence' },
     description: { type: String }
-    
+
 });
 
 const Rdv = mongoose.model('Rdv', rdvSchema);
